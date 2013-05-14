b30c2ef6c04715f5fb29d3e8db39caa25d5fe699
bkillenit <blaine.j.killen@gmail.com>
2013-05-14 16:53:32 -0500

    rails application initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lifechoicemeals::Application.config.secret_token = 'b5ea933dbee062c2608ad4213b8537d4e025f624ec65752b22ffec5b2da38619c19efd4dea81079a434fef3df4a9a6b8277970d910edd85bc913d027004f7f96'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lifechoicemeals::Application.config.session_store :cookie_store, key: '_lifechoicemeals_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lifechoicemeals::Application.config.session_store :active_record_store
