9b285b5b4f64439488c8a0c615501ab338c214e6
bkillenit <blaine.j.killen@gmail.com>
2013-05-18 18:32:52 -0500

    added thumb and medium image size methods for products images for paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price, :photo
 
   validates :name, :presence => true    
-  has_attached_file :photo
+  has_attached_file :photo, styles: {thumb: "100x100#", medium: "300x300#"}
 
 end
